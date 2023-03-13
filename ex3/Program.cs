608d97760990e5610ed1c6cceccf8f4d13853f48
drkop <satanatyn@yandex.ru>
2023-03-13 18:41:11 +0300

    3

---
+++ ex3/Program.cs
@@ -0,0 +1,56 @@
+﻿int[,,] CreateInttable()
+{
+    Console.Write("Введите высоту  ");
+    int m = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите длину  ");
+    int n = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите ширину  ");
+    int p = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    int[,,] table = new int[m,n,p];
+    return  table;
+}
+void Filltable(int[,,] table)
+{
+    int count = 10;
+    for (int i = 0; i < table.GetLength(0); i++)
+        {
+            for (int j = 0; j < table.GetLength(1); j++)
+            {             
+
+                for (int k = 0; k < table.GetLength(2); k++)
+                {     
+                    table[i, j, k] = count;
+                    count++;
+                } 
+            }
+        }
+} 
+
+void Printtable(int[,,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {                
+            Console.WriteLine();
+           for (int k = 0; k < table.GetLength(2); k++)
+            {
+                if (table[i, j, k]< 100)
+                {
+                    Console.Write(" " + table[i,j,k] + $"({i};{j};{k})");
+                }
+            }
+        }
+     Console.WriteLine();
+     }
+}
+
+int[,,] numbers = CreateInttable();
+Filltable(numbers);
+Console.WriteLine("массив: ");
+Printtable(numbers);
+
---
+++ ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите количество строк треугольника Паскаля:");
+int n = int.Parse(Console.ReadLine());
+
+void FillPascalArray (int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        int c = 1;
+        for (int k = 0; k < n - i; k++)
+        {
+            Console.Write("   ");
+        }
+ 
+        for (int j = 0; j <= i; j++)
+            {
+                Console.Write(" {0:D}    ", c);
+                c = c * (i - j) / (j + 1);
+            }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+ }
+FillPascalArray(n);
+
